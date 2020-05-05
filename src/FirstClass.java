6ce7da24d923f16090b0113363c9f115f46c02ee
JackMcCabe548 <jackmccabe628@yahoo.co.uk>
2020-05-05 15:37:40 +0100

    Version 1.50

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -6,16 +6,16 @@ public class FirstClass {
 
 // slow instantiating objects
         SecondClass s = new SecondClass();
-
         s.setFavouriteColour("Purple"); // setting attribute values
         s.setFavouriteNum(2); // Setting values attribute
         System.out.println(s.FavouriteColour);
         System.out.println(s.FavouriteNum);
 
 // Fast Constructor instantiating objects
-        ThirdClass T = new ThirdClass("Jack", 'L'); // Assign values
-        System.out.println("Name" + "\t" + "Clothing"); // Making a table
-        System.out.println(T.FirstName + "\t" + T.Size); // Call variables
+        ThirdClass FT = new ThirdClass('L');
+        System.out.println(FT.JackSize);
+
+        ThirdClass F = new ThirdClass( "newton" , 600 ); // Object with 2 arguments
 
     }
 }
